90b2488f8ccffe46d6ee1980b3ebf29309bb9424
avirupThinkbridge <50024464+avirup2512@users.noreply.github.com>
2026-06-19 23:05:28 +0530

    Set default route

--- src/App.Router.tsx
+++ src/App.Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import AuthRouter from './features/auth/auth.routes'
 import AuthLayout from './layouts/ParentAuthLayout'
 import DashboardLayout from './layouts/DashboardLayout'
@@ -10,11 +10,12 @@ function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AuthLayout/>}>
-            <Route path="auth/*" element={<AuthGuard><AuthRouter/></AuthGuard>}/>
+        <Route path="/" element={<Navigate to="auth/" replace />} />
+        <Route element={<AuthLayout />}>
+          <Route path="auth/*" element={<AuthGuard><AuthRouter /></AuthGuard>} />
         </Route>
-        <Route element={<DashboardLayout/>}>
-            <Route path="dashboard/*" element={<AuthGuard><DashboardRoute/></AuthGuard>}/>
+        <Route element={<DashboardLayout />}>
+          <Route path="dashboard/*" element={<AuthGuard><DashboardRoute /></AuthGuard>} />
         </Route>
         <Route path="*" element={<><h1>No Found.</h1></>} />
       </Routes>
