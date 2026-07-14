5b3702daeef94dde9d310cb68692c1990148dcfe
avirupThinkbridge <50024464+avirup2512@users.noreply.github.com>
2026-07-14 19:39:36 +0530

    fix Auth service base url

--- src/features/auth/service/AuthService.ts
+++ src/features/auth/service/AuthService.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://localhost:4000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://cliniure.com/api";
 
 export interface RegisterPayload {
   firstname: string;
